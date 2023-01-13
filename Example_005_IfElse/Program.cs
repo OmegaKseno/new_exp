90bbdb005249f1617f7b037d58a31b3e472439ac
Maksim.K <omega.kseno@gmail.com>
2023-01-13 23:09:14 +0300

    add if else

---
+++ Example_005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("ВВедите имя: ");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "маша")
+{
+    Console.WriteLine("здоров : " + name);
+
+}
+else
+{
+Console.WriteLine("Привет: " + name );
+
+}
