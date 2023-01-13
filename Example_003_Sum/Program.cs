43c921abde0f4e1283ed96540f670ddf17952551
Maksim.K <omega.kseno@gmail.com>
2023-01-13 22:42:50 +0300

    add random

--- Example_003_Sum/Program.cs
+++ Example_003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int numbetB = 5;
-Console.WriteLine(numberA+numbetB);
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA+numberB);
