cfc75089fa6242067817cc1b892311eb9b3b35b8
Maksim.K <omega.kseno@gmail.com>
2023-01-13 23:23:32 +0300

    add Base

---
+++ Example_006_Base/Program.cs
@@ -0,0 +1,18 @@
+﻿int a =1;
+int b = 8;
+int c = 6;
+int d = 2;
+int max = a;
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+if (d>max)
+{
+    max = d;
+}
+Console.WriteLine(max);
